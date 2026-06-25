bb2227286a5500bf9cbec34e032ad0dbd9aa944f
PKcoder2386 <khedekarpranit6@gmail.com>
2026-06-25 12:35:50 +0530

    design: updated Home and Header styles

--- src/Home.jsx
+++ src/Home.jsx
@@ -63,7 +63,7 @@ function Home({ products }) {
         <img src="./backCake1.png" className="location-img" alt="" />
         <div className="location">
           <h3>OUR RETAIL STORE</h3>
-          <p>
+          <p className="address">
             Mathura Colony, Nakhate vasti <br />
             Datta Krupa Building, Floor No.1 <br />
             <span>Pune, Maharashtra</span>
